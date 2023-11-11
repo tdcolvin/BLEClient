5bc3beeccd108a0a5623bbed7043b2930b85202e
Tom Colvin <tdcolvin@gmail.com>
2023-11-11 22:46:49 +0000

    added scan code and UI

---
+++ app/src/main/java/com/tdcolvin/bleclient/BLEClient.kt
@@ -0,0 +1,62 @@
+package com.tdcolvin.bleclient
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import androidx.annotation.RequiresPermission
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+//These fields are marked as API >= 31 in the Manifest class, so we can't use those without warning.
+//So we create our own, which prevents over-suppression of the Linter
+const val PERMISSION_BLUETOOTH_SCAN = "android.permission.BLUETOOTH_SCAN"
+const val PERMISSION_BLUETOOTH_CONNECT = "android.permission.BLUETOOTH_CONNECT"
+
+class BLEClient(context: Context) {
+
+    private val bluetooth = context.getSystemService(Context.BLUETOOTH_SERVICE)
+        as? BluetoothManager
+        ?: throw Exception("Bluetooth is not supported by this device")
+
+    val isScanning = MutableStateFlow(false)
+
+    val foundDevices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
+
+    private val scanner: BluetoothLeScanner
+        get() = bluetooth.adapter.bluetoothLeScanner
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+            result ?: return
+
+            if (!foundDevices.value.contains(result.device)) {
+                foundDevices.update { it + result.device }
+            }
+        }
+
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            super.onBatchScanResults(results)
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            super.onScanFailed(errorCode)
+            isScanning.value = false
+        }
+    }
+
+    @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
+    fun startScanning() {
+        scanner.startScan(scanCallback)
+        isScanning.value = true
+    }
+
+    @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
+    fun stopScanning() {
+        scanner.stopScan(scanCallback)
+        isScanning.value = false
+    }
+}
--- app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
+++ app/src/main/java/com/tdcolvin/bleclient/MainActivity.kt
@@ -1,16 +1,45 @@
 package com.tdcolvin.bleclient
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Application
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.tdcolvin.bleclient.ui.theme.BLEClientTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +58,156 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val ALL_BLE_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    arrayOf(
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_SCAN
+    )
+}
+else {
+    arrayOf(
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+}
+
+private fun haveAllPermissions(context: Context) =
+    ALL_BLE_PERMISSIONS
+        .all { context.checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED }
+
+@SuppressLint("MissingPermission")
 @Composable
-fun MainScreen() {
+fun MainScreen(viewModel: BLEClientViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    val context = LocalContext.current
+    var allPermissionsGranted by remember {
+        mutableStateOf (haveAllPermissions(context))
+    }
+
+    if (!allPermissionsGranted) {
+        PermissionsRequiredScreen { allPermissionsGranted = true }
+    }
+    else if (uiState.selectedDevice == null) {
+        ScanningScreen(
+            uiState.isScanning,
+            uiState.foundDevices,
+            viewModel::startScanning,
+            viewModel::stopScanning
+        )
+    }
+    else {
+        DeviceScreen()
+    }
+}
+
+@Composable
+fun PermissionsRequiredScreen(onPermissionGranted: () -> Unit) {
+    Box {
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestMultiplePermissions()
+            ) { granted ->
+                    if (granted.values.all { it }) {
+                        onPermissionGranted()
+                    }
+                }
+            Button(onClick = { launcher.launch(ALL_BLE_PERMISSIONS) }) {
+                Text("Grant Permission")
+            }
+        }
+    }
+}
+
+@Composable
+@RequiresPermission(allOf = [PERMISSION_BLUETOOTH_SCAN, PERMISSION_BLUETOOTH_CONNECT])
+fun ScanningScreen(
+    isScanning: Boolean,
+    foundDevices: List<BluetoothDevice>,
+    startScanning: () -> Unit,
+    stopScanning: () -> Unit
+) {
+    Column {
+        if (isScanning) {
+            Text("Scanning...")
+
+            Button(onClick = stopScanning) {
+                Text("Stop Scanning")
+            }
+        }
+        else {
+            Button(onClick = startScanning) {
+                Text("Start Scanning")
+            }
+        }
+
+        LazyColumn {
+            items(foundDevices) { device ->
+                Text("${device.name} / ${device.address}")
+            }
+        }
+    }
+}
+
+@Composable
+fun DeviceScreen() {
 
 }
+
+class BLEClientViewModel(application: Application): AndroidViewModel(application) {
+    private val bleClient = BLEClient(application)
+
+    private val _uiState = MutableStateFlow(BLEClientUIState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            bleClient.foundDevices.collect { devices ->
+                _uiState.update { it.copy(foundDevices = devices) }
+            }
+        }
+        viewModelScope.launch {
+            bleClient.isScanning.collect { isScanning ->
+                _uiState.update { it.copy(isScanning = isScanning) }
+            }
+        }
+    }
+
+    @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
+    fun startScanning() {
+        bleClient.startScanning()
+    }
+
+    @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
+    fun stopScanning() {
+        bleClient.stopScanning()
+    }
+
+    fun selectDevice(device: BluetoothDevice?) {
+        _uiState.update { it.copy(selectedDevice = device) }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        //when the ViewModel dies, shut down the BLE client with it
+        if (bleClient.isScanning.value) {
+            if (ActivityCompat.checkSelfPermission(
+                    getApplication(),
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                bleClient.stopScanning()
+            }
+        }
+    }
+}
+
+data class BLEClientUIState(
+    val isScanning: Boolean = false,
+    val foundDevices: List<BluetoothDevice> = emptyList(),
+    val selectedDevice: BluetoothDevice? = null
+)
